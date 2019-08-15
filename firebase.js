d01544460cf4750b52adc05cef11d42c41cba538
Anton <scoutrul@mail.ru>
2019-08-15 10:42:32 +0300

    fb conf

--- firebase.js
+++ firebase.js
@@ -2,7 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 var firebaseConfig = {
-    // ...your firebase credentials
+    apiKey: "AIzaSyA1KfnAWNp2hSu-fGXpDrOdX6jap4ICLiU",
+    authDomain: "migg-63592.firebaseapp.com",
+    databaseURL: "https://migg-63592.firebaseio.com",
+    projectId: "migg-63592",
+    storageBucket: "migg-63592.appspot.com",
+    messagingSenderId: "32087990833",
+    appId: "1:32087990833:web:c571e8790b53f035"
 };
 
 firebase.initializeApp(firebaseConfig);
