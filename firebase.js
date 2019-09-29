7faea6e96ed8e4227e136552b0e52da9ae0513d5
Anton <scoutrul@mail.ru>
2019-09-29 13:02:18 +0300

    macdev

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyA1KfnAWNp2hSu-fGXpDrOdX6jap4ICLiU",
     authDomain: "migg-63592.firebaseapp.com",
     databaseURL: "https://migg-63592.firebaseio.com",
@@ -9,7 +9,7 @@ var firebaseConfig = {
     storageBucket: "migg-63592.appspot.com",
     messagingSenderId: "32087990833",
     appId: "1:32087990833:web:c571e8790b53f035"
-};
+  };
 
 firebase.initializeApp(firebaseConfig);
 
--- src/server.js
+++ src/server.js
@@ -18,4 +18,17 @@ if (dev) {
     });
 }
 
+const jsonSend = (req, res, next) => {
+  res.json = (payload) => {
+    res.setHeader('Content-Type', 'application/json');
+    const jsonResponse = JSON.stringify(payload);
+    res.end(jsonResponse);
+  }
+
+  next()
+}
+
+polka()
+  .use(jsonSend)
+  
 export { sapper };
